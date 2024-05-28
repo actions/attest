5b17eb7cb0af08bdc8ae1f24b7d97116cff6501c
Brian DeHamer <bdehamer@github.com>
2024-05-28 13:26:14 -0700

    fix bug w/ private-signing input (#77)
    Signed-off-by: Brian DeHamer <bdehamer@github.com>

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,9 @@ const inputs: RunInputs = {
   pushToRegistry: core.getBooleanInput('push-to-registry'),
   githubToken: core.getInput('github-token'),
   // undocumented -- not part of public interface
-  privateSigning: core.getBooleanInput('private-signing'),
+  privateSigning: ['true', 'True', 'TRUE', '1'].includes(
+    core.getInput('private-signing')
+  ),
   // internal only
   batchSize: DEFAULT_BATCH_SIZE,
   batchDelay: DEFAULT_BATCH_DELAY
